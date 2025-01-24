700b0645195ce07e4e7571f7718b5809b3cc89be
Victor Hugo <victor.farias@iesde.com.br>
2025-01-24 13:32:47 -0300

    Remove linhas de depuração

--- made_in_heaven.sh
+++ made_in_heaven.sh
@@ -9,11 +9,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 # $1 is the dir name and $2 is the file name
 verifies_file_path() {
     if [ -f "$SCRIPT_DIR/$1/$2" ]; then
-        echo "Path de sucesso: $SCRIPT_DIR/$1/$2"
         source "$SCRIPT_DIR/$1/$2"
         echo "O arquivo $2 foi importado com sucesso!"
     else
-        echo "Path de fracasso: $SCRIPT_DIR/$1/$2"
         echo "Aviso: o arquivo $2 não foi encontrado!"
     fi
 }
