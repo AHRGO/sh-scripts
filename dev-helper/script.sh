0d7519be7342d37dbeeb7c4f5cac315610b6c35e
Victor Hugo <victor.030farias@gmail.com>
2025-02-20 00:03:59 -0300

    fix: :bug: Corrige bug de salvar duas questões ou mais

--- dev-helper/script.sh
+++ dev-helper/script.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
-ARQUIVO_RESPOSTAS="taskList.txt"
+# Nome do arquivo onde serão salvas as respostas.
+USER_TODO_FILE="taskList.txt"
 
-system_question() {
-    local question_to_ask=$1
-    local user_answer_to_salve=$2
+# Array para armazenar as respostas dinâmicamente
+declare -A USER_ANSWERS
+
+declare -a QUESTION_ORDER=()
+
+question_system_will_ask() {
+    local question_key=$1
+    local question_to_ask=$2
 
     echo "$question_to_ask"
     read user_answer
-    eval "$user_answer_to_salve='$user_answer'"
+    USER_ANSWERS["$question_key"]="$user_answer"
+
+    if [[ ! " ${QUESTION_ORDER[@]} " =~ "$question_key" ]]; then
+        QUESTION_ORDER+=("$question_key")
+    fi
 }
+
+save_user_answers() {
+    # Limpa o arquivo antes de iniciar o processo de salvar
+    > "$USER_TODO_FILE"
+
+    echo "====== Suas Respostas ======" > "$USER_TODO_FILE"
+
+    for question_key in "${QUESTION_ORDER[@]}"; do
+        echo "$question_key: \"${USER_ANSWERS[$question_key]}\"" >> "$USER_TODO_FILE"
+    done
+
+    echo "Todas as respostas foram salvas em $USER_TODO_FILE" 
+}
+
 
 
 project_type() {
-    system_question "Qual é o tipo de projeto que você deseja criar?" project_type_value
+    question_system_will_ask "tipo_de_projeto" "Qual é o tipo de projeto que você deseja criar?"
 }
 
-finish_and_save_answer() {
-    echo "Tipo do projeto: $project_type_value" >> "$ARQUIVO_RESPOSTAS"
-    echo "A resposta foi salva em $ARQUIVO_RESPOSTAS" 
-
+language() {
+    question_system_will_ask "linguagem_a_ser_usada" "Qual é a linguagem que você quer usar?" 
 }
+
 
 devHelper(){
+    # Limpar os arrays a cada nova execução
+    declare -A USER_ANSWERS=()
+    declare -a QUESTION_ORDER=()
+
+
     project_type
-    finish_and_save_answer
+    language
+    save_user_answers
 }
